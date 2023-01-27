c35675b0515923accc176e86d9ce06d414747bae
+ <+>
2023-01-27 18:33:43 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(arr,event) {
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+        
+           const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+        
+    }
+    return messages
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown() {
+for (let countDown = 10; countDown >= 0; countDown--)
+    console.log(countDown);
+  }
